0cd295bce6c06e5ee22c3cc2706478399f62746a
MrThomasAnderson37@gmail.com <mrthomasanderson37@gmail.com>
2025-03-26 20:02:48 -0700

    fixed vowels being doubled

--- src/content.js
+++ src/content.js
@@ -1,6 +1,6 @@
 // Import the pronunciation library (only used for certain cases)
 import {dictionary} from 'cmu-pronouncing-dictionary';
-import {englishToTengwar, specialWords, tengwarMap, vowelPhonemePatterns} from "./mappings";
+import {englishToTengwar, specialWords, tengwarMap, vowelPhonemePatterns, vowelPhonemes} from "./mappings";
 import {alignLettersToPhonemes} from "./align";
 
 // Track if Tengwar is currently enabled
@@ -309,25 +309,6 @@ function hasSilentEImproved(word, pronunciation) {
 
     // Split the pronunciation into phonemes
     const phonemes = pronunciation.split(' ');
-
-    // List of vowel phonemes in CMU dictionary
-    const vowelPhonemes = [
-        "AA", "AA0", "AA1", "AA2",
-        "AE", "AE0", "AE1", "AE2",
-        "AH", "AH0", "AH1", "AH2",
-        "AO", "AO0", "AO1", "AO2",
-        "AW", "AW0", "AW1", "AW2",
-        "AY", "AY0", "AY1", "AY2",
-        "EH", "EH0", "EH1", "EH2",
-        "ER", "ER0", "ER1", "ER2",
-        "EY", "EY0", "EY1", "EY2",
-        "IH", "IH0", "IH1", "IH2",
-        "IY", "IY0", "IY1", "IY2",
-        "OW", "OW0", "OW1", "OW2",
-        "OY", "OY0", "OY1", "OY2",
-        "UH", "UH0", "UH1", "UH2",
-        "UW", "UW0", "UW1", "UW2"
-    ];
 
     // Check if there's a final 'e' sound in the pronunciation
     const lastPhoneme = phonemes[phonemes.length - 1];
@@ -816,7 +797,7 @@ function transcribeToTengwar(text) {
         }
 
         if (!found) {
-            if (i > 0 && (processedText[i] === processedText[i - 1] ||
+            if (!'aeiou'.includes(processedText[i]) && i > 0 && (processedText[i] === processedText[i - 1] ||
                 processedText[i] === 'k' && processedText[i - 1] === 'c')) {
                 result.push(tengwarMap['doubler']);
                 i++;
